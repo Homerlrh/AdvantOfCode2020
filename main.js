404b43e138f35af4f31412f5c03922a13fe8ffc4
homer li <homer.lee.0508@gmail.com>
2020-12-05 02:39:02 -0800

    day5

--- main.js
+++ main.js
@@ -2,91 +2,54 @@ const fs = require("fs");
 
 const info = fs
 	.readFileSync("text.txt", { encoding: "utf-8" })
-	.split("\n\n")
-	.filter((x) => x)
-	.map((x) => x.split(/\s+/g))
-	.map((x) => {
-		let k = {};
-		x.map((y) => {
-			const z = y.split(":");
-			return (k[z[0]] = z[1]);
-		});
-		return k;
-	});
+	.split("\n")
+	.filter((x) => x);
 
-const validOption = ["ecl", "pid", "eyr", "hcl", "byr", "iyr", "cid", "hgt"];
+//whole range 0 ~ 127
+//f , R => 0
+//B , L=> 1
+const checkSeat = (str) => {
+	const newStr = str.split("");
+	let row = "";
+	let column = "";
+	for (let i = 0; i < str.length; i++) {
+		if (newStr[i] == "F") {
+			row += 0;
+		} else if (newStr[i] == "B") {
+			row += 1;
+		} else if (newStr[i] == "R") {
+			column += 1;
+		} else if (newStr[i] == "L") {
+			column += 0;
+		}
+	}
+	const num1 = parseInt(row, 2);
+	const num2 = parseInt(column, 2);
+	return num1 * 8 + num2;
+};
 
-const ecl = new Set(["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]);
+function range(start, end) {
+	var ans = [];
+	for (let i = start; i <= end; i++) {
+		ans.push(i);
+	}
+	return ans;
+}
 
-const findDiffInKey = (a1, a2) => {
-	const intersection = a1.filter((element) => !a2.includes(element));
-	if (intersection.length > 1) return false;
-	if (intersection.length == 0) return true;
-	if (intersection.length == 1) {
-		if (intersection[0] == "cid") return true;
-		else return false;
+const IDs = info.map((x) => checkSeat(x));
+
+const p1 = () => {
+	console.log(Math.max(...IDs));
+};
+
+const p2 = () => {
+	//my id is missing
+	//beginning and last not exist
+	//get the whole sorted list, find the not exist num
+	const listOfId = range(Math.min(...IDs), Math.max(...IDs));
+	for (let i of listOfId) {
+		if (!IDs.includes(i)) {
+			console.log(i);
+		}
 	}
 };
-
-const part1 = () => {
-	let isValid = 0;
-	info.forEach((x) => {
-		const v = Object.keys(x);
-		if (findDiffInKey(validOption, v)) {
-			isValid++;
-		}
-	});
-	console.log(isValid);
-};
-
-const check = (obj) => {
-	let isValid = false;
-
-	if (1920 <= obj["byr"] && obj["byr"] <= 2002) isValid = true;
-	else return false;
-
-	if (2010 <= obj["iyr"] && obj["iyr"] <= 2020) isValid = true;
-	else return false;
-
-	if (2020 <= obj["eyr"] && obj["eyr"] <= 2030) isValid = true;
-	else return false;
-
-	if (obj["hgt"].includes("cm") ^ obj["hgt"].includes("in")) {
-		if (obj["hgt"].includes("cm") && obj["hgt"] >= 150 && obj["hgt"] <= 193)
-			isValid = true;
-		if (obj["hgt"].includes("in") && obj["hgt"] >= 59 && obj["hgt"] <= 76)
-			isValid = true;
-	} else return false;
-
-	if (/^#[0-9a-f]{6}$/.test(obj["hcl"])) isValid = true;
-	else return false;
-
-	if (/^\d{9}$/.test(obj["pid"])) isValid = true;
-	else return false;
-
-	if (ecl.has(obj["ecl"])) isValid = true;
-	else return false;
-
-	return isValid;
-};
-
-const part2 = () => {
-	let isValid = 0;
-	let b = [];
-	info.forEach((x) => {
-		const v = Object.keys(x);
-		if (findDiffInKey(validOption, v)) {
-			b = [...b, x];
-		}
-	});
-	console.log(b.length);
-	b.forEach((x) => {
-		if (check(x)) isValid++;
-	});
-
-	console.log(isValid++);
-};
-
-part2();
-
-console.log();
