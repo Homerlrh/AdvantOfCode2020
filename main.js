08221f480886a55bff2ff87ca65b7a057ef2bd2f
homer li <homer.lee.0508@gmail.com>
2020-12-04 20:51:49 -0800

    'day4

--- main.js
+++ main.js
@@ -2,59 +2,91 @@ const fs = require("fs");
 
 const info = fs
 	.readFileSync("text.txt", { encoding: "utf-8" })
-	.split("\n")
-	.filter((x) => x);
+	.split("\n\n")
+	.filter((x) => x)
+	.map((x) => x.split(/\s+/g))
+	.map((x) => {
+		let k = {};
+		x.map((y) => {
+			const z = y.split(":");
+			return (k[z[0]] = z[1]);
+		});
+		return k;
+	});
 
-class Map {
-	constructor(map) {
-		this.map = map;
+const validOption = ["ecl", "pid", "eyr", "hcl", "byr", "iyr", "cid", "hgt"];
+
+const ecl = new Set(["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]);
+
+const findDiffInKey = (a1, a2) => {
+	const intersection = a1.filter((element) => !a2.includes(element));
+	if (intersection.length > 1) return false;
+	if (intersection.length == 0) return true;
+	if (intersection.length == 1) {
+		if (intersection[0] == "cid") return true;
+		else return false;
 	}
-
-	getPostition(x, y) {
-		return this.map[y][x % this.map[0].length];
-	}
-
-	getHeight() {
-		return this.map.length;
-	}
-}
-
-const map = new Map(info.map((line) => [...line]));
-
-const checkTree = (a, b) => {
-	let x = 0;
-	let y = 0;
-	let trees = 0;
-
-	while (y < map.getHeight()) {
-		const currtent = map.getPostition(x, y);
-		if (currtent == "#") trees++;
-		y += b;
-		x += a;
-	}
-	return trees;
 };
 
 const part1 = () => {
-	console.log(checkTree(3, 1));
+	let isValid = 0;
+	info.forEach((x) => {
+		const v = Object.keys(x);
+		if (findDiffInKey(validOption, v)) {
+			isValid++;
+		}
+	});
+	console.log(isValid);
 };
 
-const b = [
-	{ x: 1, y: 1 },
-	{ x: 3, y: 1 },
-	{ x: 5, y: 1 },
-	{ x: 7, y: 1 },
-	{ x: 1, y: 2 },
-];
+const check = (obj) => {
+	let isValid = false;
+
+	if (1920 <= obj["byr"] && obj["byr"] <= 2002) isValid = true;
+	else return false;
+
+	if (2010 <= obj["iyr"] && obj["iyr"] <= 2020) isValid = true;
+	else return false;
+
+	if (2020 <= obj["eyr"] && obj["eyr"] <= 2030) isValid = true;
+	else return false;
+
+	if (obj["hgt"].includes("cm") ^ obj["hgt"].includes("in")) {
+		if (obj["hgt"].includes("cm") && obj["hgt"] >= 150 && obj["hgt"] <= 193)
+			isValid = true;
+		if (obj["hgt"].includes("in") && obj["hgt"] >= 59 && obj["hgt"] <= 76)
+			isValid = true;
+	} else return false;
+
+	if (/^#[0-9a-f]{6}$/.test(obj["hcl"])) isValid = true;
+	else return false;
+
+	if (/^\d{9}$/.test(obj["pid"])) isValid = true;
+	else return false;
+
+	if (ecl.has(obj["ecl"])) isValid = true;
+	else return false;
+
+	return isValid;
+};
 
 const part2 = () => {
-	let result = 1;
+	let isValid = 0;
+	let b = [];
+	info.forEach((x) => {
+		const v = Object.keys(x);
+		if (findDiffInKey(validOption, v)) {
+			b = [...b, x];
+		}
+	});
+	console.log(b.length);
+	b.forEach((x) => {
+		if (check(x)) isValid++;
+	});
 
-	for (const i of b) {
-		result *= checkTree(i.x, i.y);
-	}
-
-	console.log(result);
+	console.log(isValid++);
 };
 
 part2();
+
+console.log();
