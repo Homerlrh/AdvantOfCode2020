79e19b3ec926a2f275d80d7f4d22144d7aad4d98
homer li <homer.lee.0508@gmail.com>
2020-12-08 00:55:19 -0800

    day8

--- main.js
+++ main.js
@@ -1,60 +1,71 @@
 const fs = require("fs");
 
-const info = fs.readFileSync("text.txt", { encoding: "utf-8" }).split("\n");
+const info = fs.readFileSync("text.txt", { encoding: "utf-8" }).split("\r\n");
 
 const p1 = () => {
-	const obj = {};
-
-	info.forEach((x) => {
-		const [a, bag, contain] = /(\w+ \w+) bags contain (.*)\./.exec(x);
-		const containBag =
-			contain !== "no other bags"
-				? contain.split(", ").map((other) => /(\w+ \w+) bags?/.exec(other)[1])
-				: [];
-
-		obj[bag] = new Set(containBag);
-	});
-
-	const insertColor = (bag) => {
-		let colors = [...obj[bag]];
-		for (const color of [...obj[bag]]) {
-			colors = [...colors, ...insertColor(color)];
+	const isVisited = [];
+	let currentIndex = 0;
+	let acc = 0;
+	while (!isVisited.includes(currentIndex)) {
+		isVisited.push(currentIndex);
+		const [k, v] = info[currentIndex].split(" ");
+		if (k == "nop") {
+			currentIndex++;
+		} else {
+			const num = parseInt(v);
+			if (k == "acc") {
+				acc += num;
+				currentIndex++;
+			} else {
+				currentIndex += num;
+			}
 		}
-		return colors;
-	};
-	return Object.keys(obj)
-		.map((x) => insertColor(x))
-		.filter((x) => x.includes("shiny gold")).length;
+	}
+	console.log(acc);
 };
 
 const p2 = () => {
-	const obj = {};
-
-	info.forEach((x) => {
-		const [a, bag, contain] = /(\w+ \w+) bags contain (.*)\./.exec(x);
-		const containBag =
-			contain !== "no other bags"
-				? contain.split(", ").map((other) => {
-						const [, units, color] = /(\d+) (\w+ \w+) bags?/.exec(other);
-						return { units: parseInt(units), color };
-				  })
-				: [];
-		obj[bag] = new Set(containBag);
+	// just need to change one at a time
+	// get all the combo
+	const p2Array = info.map((x, i, a) => {
+		const array = [...a];
+		if (x.includes("nop")) {
+			x = x.replace("nop", "jmp");
+		} else if (x.includes("jmp")) {
+			x = x.replace("jmp", "nop");
+		} else {
+			x = x;
+		}
+		array[i] = x;
+		return array;
 	});
 
-	const b = new Map();
-	for (const i in obj) {
-		b.set(i, [...obj[i]]);
-	}
-	const calBag = (bag) => {
-		let total = 0;
-		for (const { color, units } of b.get(bag)) {
-			total += units + units * calBag(color);
+	p2Array.forEach((x) => {
+		let isVisited = [];
+		let currentIndex = 0;
+		let acc = 0;
+		while (!isVisited.includes(currentIndex)) {
+			isVisited.push(currentIndex);
+			const [k, v] = x[currentIndex].split(" ");
+			if (k == "nop") {
+				currentIndex++;
+			} else {
+				const num = parseInt(v);
+				if (k == "acc") {
+					acc += num;
+					currentIndex++;
+				} else {
+					currentIndex += num;
+				}
+			}
+			//get travel from top to bottom
+			if (currentIndex == x.length) {
+				console.log(acc);
+				break;
+			}
 		}
-		return total;
-	};
-
-	return calBag("shiny gold");
+	});
 };
 
-console.log(p1(), p2());
+p1();
+p2();
